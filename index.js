368761436e3b097ee1e313a55dc3fbd3729d3a4a
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2019-12-09 15:36:30 +0200

    add custom middleware

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  console.log("Logging...");
+  next();
+});
+
+app.use(function(req, res, next) {
+  console.log("Authenticating...");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
 app.listen(3000, () => console.log("Listening on port 3000..."));
